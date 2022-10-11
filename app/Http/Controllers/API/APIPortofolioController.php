89036f3e7ab8126a7c141301c7103c6daebda93d
fiqih16 <fiqih1666@gmail.com>
2022-10-12 00:09:20 +0700

    API Add Img Portofolio

--- app/Http/Controllers/API/APIPortofolioController.php
+++ app/Http/Controllers/API/APIPortofolioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Portofolio\StorePortofolioRequest;
+use App\Http\Requests\Portofolio\StorePortofolioImgRequest;
 use App\Services\Portofolio\PortofolioService;
 use App\Models\Category;
 use Exception;
@@ -24,6 +25,17 @@ class APIPortofolioController extends BaseController
             $request->validated();
             $portofolio = $this->portofolioService->createPortofolio($request);
             return $this->sendResponse('Successfully Created Portofolio', 200, $portofolio);
+        } catch (Exception $err) {
+            return $this->sendError('Fail', 422, $err->getMessage());
+        }
+    }
+
+    public function storeImage(StorePortofolioImgRequest $request)
+    {
+        try {
+            $request->validated();
+            $portofolio_image = $this->portofolioService->createPortofolioImage($request);
+            return $this->sendResponse('Successfully Created Portofolio Image', 200, $portofolio_image);
         } catch (Exception $err) {
             return $this->sendError('Fail', 422, $err->getMessage());
         }
--- app/Services/Portofolio/PortofolioService.php
+++ app/Services/Portofolio/PortofolioService.php
@@ -2,9 +2,10 @@
 
 namespace App\Services\Portofolio;
 
+use App\Http\Requests\Portofolio\StorePortofolioImgRequest;
 use App\Http\Requests\Portofolio\StorePortofolioRequest;
-use App\Models\Category;
 use App\Models\Portofolio;
+use App\Models\Portofolio_image;
 use App\Repositories\Portofolio\PortofolioRepository;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -32,6 +33,44 @@ class PortofolioService
         $portofolio->user_id = Auth::user()->id;
 
         return $this->portofolioRepository->insert($portofolio);
+    }
+
+    public function createPortofolioImage(StorePortofolioImgRequest $request)
+    {
+        $portofolio_image = new Portofolio_image();
+        $portofolio_image->portofolio_id = $request->portofolio_id;
+
+        // cek apakah ada file yang diupload
+        if ($request->hasFile('file_name')) {
+            // ambil file yang diupload
+            $uploaded_file = $request->file('file_name');
+            // mengambil extension file
+            $extension = $uploaded_file->getClientOriginalExtension();
+            // membuat nama file random berikut extension
+            $filename = md5(time()) . '.' . $extension;
+            // menyimpan file ke folder img/portofolio
+            $request->file('file_name')->move('img/portofolio', $filename);
+            // mengisi field file_name di portofolio_image dengan filename yang baru dibuat
+            $portofolio_image->file_name = $filename;
+        } else {
+            $portofolio_image->file_name = null;
+        }
+
+        // jika portofolio_image berdasarkan id portofolio pertama kali dibuat, maka is_primary = 1
+        if ($this->portofolioRepository->getPortofolioImageByPortofolioId($request->portofolio_id)->count() > 0) {
+            $portofolio_image->is_primary = 0;
+        } else {
+            $portofolio_image->is_primary = 1;
+        }
+
+        // jika portofolio_image berdasarkan id portofolio tidak ada yang is_primary = 1, maka is_primary = 1 untuk portofolio_image yang baru dibuat
+        if ($this->portofolioRepository->getPortofolioImageByPortofolioId($request->portofolio_id)->where('is_primary', 1)->count() == 0) {
+            $portofolio_image->is_primary = 1;
+        } else {
+            $portofolio_image->is_primary = 0;
+        }
+
+        return $this->portofolioRepository->insertImage($portofolio_image);
     }
 }
 
